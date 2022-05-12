784635557f0b65cee06d6943a725828cdfda179b
charkai <89690267+charkai@users.noreply.github.com>
2022-05-12 10:23:54 +1000

    Adding more comments to game.py

--- game.py
+++ game.py
@@ -16,19 +16,20 @@ from config import Config
 
 class Settings:
     def __init__(self):
+        #size of game, and then size of individual grids. 
         self.width = 28
         self.height = 28
         self.rect_len = 30
-        #size of game, and then size of individual grids. 
-        # self.width = 52
-        # self.height = 52
-        # self.rect_len = 15
+        
+
 
 class Strawberry():
     def __init__(self, settings, parent):
+        #  Strawberry is initialised associated with the settings, as well as a 'parent' game that it is loaded into 
         self.settings = settings
         self.parent = parent
 
+        #  The type of strawberry is randomly allocated, and its associated image is fetched
         self.style = str(random.randint(1, 8))
         path = self.parent.src + "/styles/" + self.parent.style + "/images/"
         self.image = pygame.image.load(path + 'food' + str(self.style) + '.bmp')        
@@ -39,28 +40,34 @@ class Strawberry():
         path = self.parent.src + "/styles/" + self.parent.style + "/images/"
         self.image = pygame.image.load(path + 'food' + str(self.style) + '.bmp')                
         print("called!")
+
+        # The strawberry can spawn in randomly at any location in the map 
         self.position[0] = random.randint(0, int(self.parent.config.settings["mapX"])-1)
         self.position[1] = random.randint(0, int(self.parent.config.settings["mapY"])-1)
 
-
+        # If the strawberry accidentally spawns inside the snake, the function is re-called
         if (self.position in self.parent.snake.segmentd[:-1]) or not self.parent.map.tiles[self.position[1]][self.position[0]].true_empty:
             self.random_pos()
 
     def blit(self, screen, x0, y0):
+        #  The strawberry is blitted onto the screen in it's random position 
         x_f = (self.position[0] + x0)*self.settings.rect_len
         y_f = (self.position[1] + y0)*self.settings.rect_len
         screen.blit(self.image, (x_f, y_f))
         pygame.display.update(pygame.Rect(x_f, y_f, self.settings.rect_len, self.settings.rect_len))
    
     def initialize(self):
+        # The initial position for the strawberry is default
         self.position = [15, 10]
     
     def playSound(self):
+        # When astrawberry is eaten, the game plays a sound
         eat_sound = pygame.mixer.Sound('./sound/eat.wav')
         pygame.mixer.Sound.play(eat_sound)
  
 class Game:
     def __init__(self, game_data, real_location):
+        # Each game is initialised with game data, location, settings, a snake, a snake clone, and strawberries
         self.src = game_data
         self.srcreal = real_location
         self.style = '0'
@@ -69,6 +76,8 @@ class Game:
         self.snake = Snake(self)
         self.snake_clone = Snake(self, clone=True)
         self.strawberry = Strawberry(self.settings, self)
+    
+        # This dictionary helps in transforming integers to directions
         self.move_dict = {0 : 'up',
                           1 : 'down',
                           2 : 'left',
@@ -76,6 +85,7 @@ class Game:
                           -1: 'none'}
 
     def reset_img_source(self): 
+        #  Fetching the images for the map - all of the tiles and visuals are sourced here
         path = self.src + "/styles/" + self.style + "/images/"
         self.tile_img = pygame.transform.scale(pygame.image.load(path + 'tile.bmp'), (self.settings.rect_len, self.settings.rect_len))
         self.space_img = pygame.transform.scale(pygame.image.load(path + 'space.bmp'), (self.settings.rect_len, self.settings.rect_len))
@@ -92,19 +102,24 @@ class Game:
         #update visuals at the start of the game
         self.reset_img_source()
         self.snake.reset_img_source()
-        #set config. This has a bunch of options that control stuff.
+
+        #set config. This ultimately controls a lot of the functions of the game
         self.config = Config(parent=self, mapdir=mapdir)
+
         #set map
         self.map = Map(parent=self, mapdir=mapdir)
+
         #set snake
         self.snake.initialize(mapdir)
         self.snake_clone.init = False
         self.snake_clone.segmentd = []
         self.snake_clone.score = 0
+
         #set stawberry if it exists.
         self.strawberry.random_pos()
     
     def direction_to_int(self, direction):
+        # Parses through the move_dict dictionary, transforming a direction (string) to an integer 
         direction_dict = {value : key for key,value in self.move_dict.items()}
         return direction_dict[direction]
         
@@ -112,8 +127,9 @@ class Game:
         move_dict = self.move_dict
 
         change_direction = move_dict[move]
-        #this translates the number back to the string again. Kinda redundant tbh.
+        #translates the number back to the string again
 
+        #  Direction is changed based off of move - and checks whether the snake is already moving in this direction
         if change_direction == 'right' and not self.snake.segments[1] == [1, 0]:
             self.snake.facing = change_direction
         if change_direction == 'left' and not self.snake.segments[1] == [-1, 0]:
@@ -123,6 +139,7 @@ class Game:
         if change_direction == 'down' and not self.snake.segments[1] == [0, 1]:
             self.snake.facing = change_direction
 
+        # Clone mechanics- if the snake moves, so does the clone
         if self.snake_clone.init:
             if change_direction == 'right' and not self.snake_clone.segments[1] == [1, 0]:
                 self.snake_clone.facing = change_direction
@@ -133,7 +150,7 @@ class Game:
             if change_direction == 'down' and not self.snake_clone.segments[1] == [0, 1]:
                 self.snake_clone.facing = change_direction
         
-        #state, replace = self.snake.update()
+        #  The new states (the last tail segment and the changes to the body) of the snake (and its clone ) are returned  
         if self.snake_clone.init:
             state1, replace1 = self.snake_clone.update()
         else:
@@ -143,37 +160,51 @@ class Game:
         return state, state1, replace, replace1
     
     def game_end(self):
+    
         end = False
         # if the snake hits the edge of the border, the snake dies
         if self.snake.position[0] >= (self.settings.width-2) or self.snake.position[0] < 2:
             end = True
         if self.snake.position[1] >= (self.settings.height-2) or self.snake.position[1] < 2:
             end = True
+        
+        # If the snake hits itself, it dies 
         if self.snake.segments[0] in self.snake.segments[1:]:
             end = True
         return end
     
     def blit_score(self, color, screen):
+        #  The score is blitted onto the screen
         font = pygame.font.Font(self.src + '/arial.ttf', 25)
         text = font.render('Score: ' + str(self.snake.score + self.snake_clone.score), True, color)
         screen.blit(text, (0, 0))
 
     def blit_map(self, rect_len, screen): 
+        #  Blits the map onto the screen 
         x0 = int(self.config.settings["xOffset"])
         y0 = int(self.config.settings["yOffset"])
+
+        # Parses through the multidimensional list of tiles stored in the map object(multidimensional due to row-column)
         for i in range(0, int(self.config.settings["mapX"])):
             for k in range(0, int(self.config.settings["mapY"])):
+                
                 tile = self.map.tiles[k][i]
                 if tile.type == "Other":
                     pass
+
+                # If the tile type is solid, this is blitted to the screen
                 elif tile.type == "Solid":
                     screen.blit(self.tile_img, ((i + x0)*rect_len, (k + y0)*rect_len))
                     for j in range(4):
                         if tile.wrap_plate & (1 << j):
+                            # If the tile has a wrap plate, this is blitted too
                             screen.blit(pygame.transform.rotate(self.wrap_img, j*90), ((i + x0)*rect_len, (k + y0)*rect_len))
                     for j in range(4):
                         if tile.pad_clone & (1 << j):
+                            # If the tile has a pad plate, this is blitted 
                             screen.blit(pygame.transform.rotate(self.pad_img, j*90), ((i + x0)*rect_len, (k + y0)*rect_len))
+                
+                # An empty space tile is generated in the map if the tile is of type empty
                 elif tile.type == "Empty": 
                     screen.blit(self.space_img, ((i + x0)*rect_len, (k + y0)*rect_len))
                 else:
@@ -182,7 +213,7 @@ class Game:
     def blit_features(self, rect_len, screen):
         x0 = int(self.config.settings["xOffset"])
         y0 = int(self.config.settings["yOffset"])
-        #pressure plates
+        #Pressure plates
         for location in self.map.goals:
             x_f, y_f = (location[0] + x0)*rect_len, (location[1] + y0)*rect_len
             if (location in self.snake.segmentd) or (location in self.snake_clone.segmentd):
